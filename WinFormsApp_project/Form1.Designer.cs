571800ade6e4d60e37967586e80f09406e7618d1
Mattioo7 <mati.chudek@op.pl>
2022-04-07 15:01:53 +0200

    Initial commit

---
+++ WinFormsApp_project/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace WinFormsApp_project
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.LanguageBox = new System.Windows.Forms.GroupBox();
+			this.EnglishLanguageButton = new System.Windows.Forms.Button();
+			this.PolishLanguageButton = new System.Windows.Forms.Button();
+			this.EditionBox = new System.Windows.Forms.GroupBox();
+			this.DeleteButton = new System.Windows.Forms.Button();
+			this.colorField = new System.Windows.Forms.Label();
+			this.ColorButton = new System.Windows.Forms.Button();
+			this.ImportBox = new System.Windows.Forms.GroupBox();
+			this.LoadButton = new System.Windows.Forms.Button();
+			this.SaveButton = new System.Windows.Forms.Button();
+			this.workingArea = new System.Windows.Forms.PictureBox();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.tableLayoutPanel2.SuspendLayout();
+			this.LanguageBox.SuspendLayout();
+			this.EditionBox.SuspendLayout();
+			this.ImportBox.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.workingArea)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.workingArea, 0, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 561);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.ColumnCount = 1;
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel2.Controls.Add(this.LanguageBox, 0, 1);
+			this.tableLayoutPanel2.Controls.Add(this.EditionBox, 0, 0);
+			this.tableLayoutPanel2.Controls.Add(this.ImportBox, 0, 2);
+			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(630, 3);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 3;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(151, 555);
+			this.tableLayoutPanel2.TabIndex = 0;
+			// 
+			// LanguageBox
+			// 
+			this.LanguageBox.Controls.Add(this.EnglishLanguageButton);
+			this.LanguageBox.Controls.Add(this.PolishLanguageButton);
+			this.LanguageBox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.LanguageBox.Location = new System.Drawing.Point(3, 258);
+			this.LanguageBox.Name = "LanguageBox";
+			this.LanguageBox.Size = new System.Drawing.Size(145, 144);
+			this.LanguageBox.TabIndex = 0;
+			this.LanguageBox.TabStop = false;
+			this.LanguageBox.Text = "Język";
+			// 
+			// EnglishLanguageButton
+			// 
+			this.EnglishLanguageButton.Enabled = false;
+			this.EnglishLanguageButton.Location = new System.Drawing.Point(7, 74);
+			this.EnglishLanguageButton.Name = "EnglishLanguageButton";
+			this.EnglishLanguageButton.Size = new System.Drawing.Size(138, 46);
+			this.EnglishLanguageButton.TabIndex = 1;
+			this.EnglishLanguageButton.Text = "Angielski";
+			this.EnglishLanguageButton.UseVisualStyleBackColor = true;
+			// 
+			// PolishLanguageButton
+			// 
+			this.PolishLanguageButton.Enabled = false;
+			this.PolishLanguageButton.Location = new System.Drawing.Point(6, 22);
+			this.PolishLanguageButton.Name = "PolishLanguageButton";
+			this.PolishLanguageButton.Size = new System.Drawing.Size(138, 46);
+			this.PolishLanguageButton.TabIndex = 0;
+			this.PolishLanguageButton.Text = "Polski";
+			this.PolishLanguageButton.UseVisualStyleBackColor = true;
+			// 
+			// EditionBox
+			// 
+			this.EditionBox.Controls.Add(this.DeleteButton);
+			this.EditionBox.Controls.Add(this.colorField);
+			this.EditionBox.Controls.Add(this.ColorButton);
+			this.EditionBox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.EditionBox.Location = new System.Drawing.Point(3, 3);
+			this.EditionBox.Name = "EditionBox";
+			this.EditionBox.Size = new System.Drawing.Size(145, 249);
+			this.EditionBox.TabIndex = 1;
+			this.EditionBox.TabStop = false;
+			this.EditionBox.Text = "Edycja";
+			// 
+			// DeleteButton
+			// 
+			this.DeleteButton.Enabled = false;
+			this.DeleteButton.Location = new System.Drawing.Point(7, 51);
+			this.DeleteButton.Name = "DeleteButton";
+			this.DeleteButton.Size = new System.Drawing.Size(132, 23);
+			this.DeleteButton.TabIndex = 2;
+			this.DeleteButton.Text = "Usuń wierzchołek";
+			this.DeleteButton.UseVisualStyleBackColor = true;
+			this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+			// 
+			// colorField
+			// 
+			this.colorField.AutoSize = true;
+			this.colorField.Location = new System.Drawing.Point(87, 26);
+			this.colorField.MinimumSize = new System.Drawing.Size(40, 15);
+			this.colorField.Name = "colorField";
+			this.colorField.Size = new System.Drawing.Size(40, 15);
+			this.colorField.TabIndex = 1;
+			// 
+			// ColorButton
+			// 
+			this.ColorButton.Location = new System.Drawing.Point(6, 22);
+			this.ColorButton.Name = "ColorButton";
+			this.ColorButton.Size = new System.Drawing.Size(75, 23);
+			this.ColorButton.TabIndex = 0;
+			this.ColorButton.Text = "Kolor";
+			this.ColorButton.UseVisualStyleBackColor = true;
+			this.ColorButton.Click += new System.EventHandler(this.ColorButton_Click);
+			// 
+			// ImportBox
+			// 
+			this.ImportBox.Controls.Add(this.LoadButton);
+			this.ImportBox.Controls.Add(this.SaveButton);
+			this.ImportBox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.ImportBox.Location = new System.Drawing.Point(3, 408);
+			this.ImportBox.Name = "ImportBox";
+			this.ImportBox.Size = new System.Drawing.Size(145, 144);
+			this.ImportBox.TabIndex = 2;
+			this.ImportBox.TabStop = false;
+			this.ImportBox.Text = "Import/Export";
+			// 
+			// LoadButton
+			// 
+			this.LoadButton.Enabled = false;
+			this.LoadButton.Location = new System.Drawing.Point(6, 83);
+			this.LoadButton.Name = "LoadButton";
+			this.LoadButton.Size = new System.Drawing.Size(138, 46);
+			this.LoadButton.TabIndex = 2;
+			this.LoadButton.Text = "Wczytaj";
+			this.LoadButton.UseVisualStyleBackColor = true;
+			// 
+			// SaveButton
+			// 
+			this.SaveButton.Enabled = false;
+			this.SaveButton.Location = new System.Drawing.Point(6, 31);
+			this.SaveButton.Name = "SaveButton";
+			this.SaveButton.Size = new System.Drawing.Size(138, 46);
+			this.SaveButton.TabIndex = 1;
+			this.SaveButton.Text = "Zapisz";
+			this.SaveButton.UseVisualStyleBackColor = true;
+			// 
+			// workingArea
+			// 
+			this.workingArea.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.workingArea.Location = new System.Drawing.Point(3, 3);
+			this.workingArea.Name = "workingArea";
+			this.workingArea.Size = new System.Drawing.Size(621, 555);
+			this.workingArea.TabIndex = 1;
+			this.workingArea.TabStop = false;
+			this.workingArea.MouseDown += new System.Windows.Forms.MouseEventHandler(this.workingArea_MouseDown);
+			this.workingArea.MouseMove += new System.Windows.Forms.MouseEventHandler(this.workingArea_MouseMove);
+			this.workingArea.MouseUp += new System.Windows.Forms.MouseEventHandler(this.workingArea_MouseUp);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(784, 561);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.MinimumSize = new System.Drawing.Size(800, 600);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel2.ResumeLayout(false);
+			this.LanguageBox.ResumeLayout(false);
+			this.EditionBox.ResumeLayout(false);
+			this.EditionBox.PerformLayout();
+			this.ImportBox.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.workingArea)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private System.Windows.Forms.GroupBox LanguageBox;
+		private System.Windows.Forms.GroupBox EditionBox;
+		private System.Windows.Forms.GroupBox ImportBox;
+		private System.Windows.Forms.PictureBox workingArea;
+		private System.Windows.Forms.Button EnglishLanguageButton;
+		private System.Windows.Forms.Button PolishLanguageButton;
+		private System.Windows.Forms.Button LoadButton;
+		private System.Windows.Forms.Button SaveButton;
+		private System.Windows.Forms.Button ColorButton;
+		private System.Windows.Forms.Label colorField;
+		private System.Windows.Forms.Button DeleteButton;
+	}
+}
+
---
+++ WinFormsApp_project/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp_project
+{
+	public partial class Form1 : Form
+	{
+		private Bitmap drawArea;
+		private const int RADIUS = 18;
+		private Pen pen;
+		private Pen whitePen;
+		private Pen dashedPen;
+		private Color vertColor = Color.Black;
+		private List<(Point point, Color color)> verts;
+		private int vertNumber = 0;
+		private Point chosenVert;
+		private int chosenVertNR;
+		private Point mousePosition;
+		private bool mouseIsDown;
+		private Point clickedVert;
+		private int clickedVertNR;
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			drawArea = new Bitmap(workingArea.Size.Width, workingArea.Size.Height);
+			workingArea.Image = drawArea;
+			using (Graphics g = Graphics.FromImage(drawArea))
+			{
+				g.Clear(Color.LightBlue);
+			}
+
+			colorField.BackColor = vertColor;
+			verts = new List<(Point, Color)>();
+			pen = new Pen(Color.Black, 3);
+			dashedPen = new Pen(Brushes.Black, 3);
+			whitePen = new Pen(Brushes.White, 3);
+			dashedPen.DashPattern = new float[] { 3, 3 };
+			whitePen.DashPattern = new float[] { 3, 3 };
+			chosenVert = new Point(-1, -1);
+			chosenVertNR = -1;
+			mousePosition = new Point(-1, -1);
+			clickedVert = new Point(-1, -1);
+			clickedVertNR = -1;
+		}
+
+		private void Form1_SizeChanged(object sender, EventArgs e)
+		{
+			int it = 1;
+			var newsize = tableLayoutPanel1.GetControlFromPosition(0, 0).Size;
+			drawArea = new Bitmap(newsize.Width, newsize.Height);
+			workingArea.Image = drawArea;
+			using (Graphics g = Graphics.FromImage(drawArea))
+			{
+				g.Clear(Color.LightBlue);
+			}
+
+			foreach (var v in verts)
+			{
+				using (Graphics g = Graphics.FromImage(drawArea))
+				{
+					g.DrawEllipse(new Pen(v.color, 3), v.point.X - RADIUS, v.point.Y - RADIUS, RADIUS * 2, RADIUS * 2);
+					g.DrawString(it.ToString(),
+						new Font("Ink Free", 12),
+						new SolidBrush(v.color),
+						v.point.X, v.point.Y,
+						new StringFormat()
+						{ Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+				}
+				++it;
+			}
+
+			workingArea.Refresh();
+		}
+
+		private void workingArea_MouseDown(object sender, MouseEventArgs e)
+		{
+			bool flag = false;
+
+			if (e.Button == MouseButtons.Left)
+			{
+				using (Graphics g = Graphics.FromImage(drawArea))
+				{
+					if (vertNumber > 0)
+					{
+						foreach (var v in verts)
+						{
+							int yDiff = Math.Abs(v.point.Y - e.Y);
+							int xDiff = Math.Abs(v.point.X - e.X);
+
+							if ((yDiff < 2 * RADIUS) && (xDiff < 2 * RADIUS))
+							{
+								flag = true;
+							}
+						}
+					}
+
+					if (flag == true) return;
+
+					g.DrawEllipse(pen, e.X - RADIUS, e.Y - RADIUS, RADIUS * 2, RADIUS * 2);
+					g.DrawString(vertNumber.ToString(),
+						new Font("Ink Free", 12),
+						new SolidBrush(vertColor),
+						e.X, e.Y,
+						new StringFormat()
+						{ Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+				}
+				verts.Add((new Point(e.X, e.Y), vertColor));
+				vertNumber++;
+				workingArea.Refresh();
+			}
+			else if (e.Button == MouseButtons.Right)
+			{
+				int yDiffBest = int.MaxValue;
+				int xDiffBest = int.MaxValue;
+				int iBest = -1;
+
+				int i = 0;
+
+				foreach (var v in verts)
+				{
+					int yDiff = Math.Abs(v.point.Y - e.Y);
+					int xDiff = Math.Abs(v.point.X - e.X);
+
+					if ((yDiff < 2 * RADIUS) && (xDiff < 2 * RADIUS) && yDiff < yDiffBest && xDiff < xDiffBest)
+					{
+						xDiffBest = xDiff;
+						yDiffBest = yDiff;
+						iBest = i;
+					}
+					i++;
+				}
+
+				if (chosenVert.X != -1)
+				{
+					int it = 0;
+					foreach (var v in verts)
+					{
+						using (Graphics g = Graphics.FromImage(drawArea))
+						{
+							g.DrawEllipse(new Pen(v.color, 3), v.point.X - RADIUS, v.point.Y - RADIUS, RADIUS * 2, RADIUS * 2);
+							g.DrawString(it.ToString(),
+								new Font("Ink Free", 12),
+								new SolidBrush(v.color),
+								v.point.X, v.point.Y,
+								new StringFormat()
+								{ Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+						}
+						++it;
+					}
+
+					chosenVert.X = -1;
+					chosenVertNR = -1;
+					DeleteButton.Enabled = false;
+					workingArea.Refresh();
+				}
+
+				if (iBest != -1)
+				{
+					using (Graphics g = Graphics.FromImage(drawArea))
+					{
+						chosenVert.X = verts[iBest].point.X;
+						chosenVert.Y = verts[iBest].point.Y;
+						chosenVertNR = i;
+						g.DrawEllipse(whitePen, chosenVert.X - RADIUS, chosenVert.Y - RADIUS, RADIUS * 2, RADIUS * 2);
+						workingArea.Refresh();
+					}
+
+					DeleteButton.Enabled = true;
+				}
+				workingArea.Refresh();
+			}
+			else if (e.Button == MouseButtons.Middle)
+			{
+
+				int yDiffBest = int.MaxValue;
+				int xDiffBest = int.MaxValue;
+
+				int i = 0;
+
+				foreach (var v in verts)
+				{
+					int yDiff = Math.Abs(v.point.Y - e.Y);
+					int xDiff = Math.Abs(v.point.X - e.X);
+
+					if ((yDiff < 2 * RADIUS) && (xDiff < 2 * RADIUS) && yDiff < yDiffBest && xDiff < xDiffBest)
+					{
+						xDiffBest = xDiff;
+						yDiffBest = yDiff;
+						clickedVertNR = i;
+						clickedVert.Y = v.point.Y;
+						clickedVert.X = v.point.X;
+						mouseIsDown = true;
+					}
+					i++;
+				}
+			}
+		}
+
+		private void ColorButton_Click(object sender, EventArgs e)
+		{
+			ColorDialog colorDialog = new ColorDialog();
+			if (colorDialog.ShowDialog() == DialogResult.OK)
+			{
+				vertColor = colorDialog.Color;
+				pen.Color = vertColor;
+				colorField.BackColor = vertColor;
+			}
+
+			verts[chosenVertNR] = (new Point(verts[chosenVertNR].point.X, verts[chosenVertNR].point.Y), vertColor);
+			var v = verts[chosenVertNR];
+			using (Graphics g = Graphics.FromImage(drawArea))
+			{
+				g.DrawEllipse(new Pen(v.color, 3), v.point.X - RADIUS, v.point.Y - RADIUS, RADIUS * 2, RADIUS * 2);
+				g.DrawString(chosenVertNR.ToString(),
+					new Font("Ink Free", 12),
+					new SolidBrush(v.color),
+					v.point.X, v.point.Y,
+					new StringFormat()
+					{ Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+				g.DrawEllipse(whitePen, chosenVert.X - RADIUS, chosenVert.Y - RADIUS, RADIUS * 2, RADIUS * 2);
+			}
+
+			workingArea.Refresh();
+		}
+
+		private void DeleteButton_Click(object sender, EventArgs e)
+		{
+			int it = 0;
+			foreach (var v in verts)
+			{
+				if (v.point.X == chosenVert.X && v.point.Y == chosenVert.Y)
+				{
+					break;
+				}
+				it++;
+			}
+			verts.RemoveAt(it);
+			it = 0;
+			using (Graphics g = Graphics.FromImage(drawArea))
+			{
+				g.Clear(Color.LightBlue);
+				foreach (var v in verts)
+				{
+					g.DrawEllipse(new Pen(v.color, 3), v.point.X - RADIUS, v.point.Y - RADIUS, RADIUS * 2, RADIUS * 2);
+					g.DrawString(it.ToString(),
+						new Font("Ink Free", 12),
+						new SolidBrush(v.color),
+						v.point.X, v.point.Y,
+						new StringFormat()
+						{ Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+					++it;
+				}
+			}
+			chosenVert.X = -1;
+			chosenVertNR = -1;
+			DeleteButton.Enabled = false;
+			vertNumber--;
+			workingArea.Refresh();
+		}
+
+		private void workingArea_MouseMove(object sender, MouseEventArgs e)
+		{
+			mousePosition.X = e.X;
+			mousePosition.Y = e.Y;
+
+			if (mouseIsDown == true)
+			{
+				if (clickedVertNR != -1)
+				{
+					using (Graphics g = Graphics.FromImage(drawArea))
+					{
+						g.Clear(Color.LightBlue);
+						g.DrawEllipse(pen, mousePosition.X - RADIUS, mousePosition.Y - RADIUS, RADIUS * 2, RADIUS * 2);
+					}
+					int it = 0;
+					foreach (var v in verts)
+					{
+						using (Graphics g = Graphics.FromImage(drawArea))
+						{
+							g.DrawEllipse(new Pen(v.color, 3), v.point.X - RADIUS, v.point.Y - RADIUS, RADIUS * 2, RADIUS * 2);
+							g.DrawString(it.ToString(),
+								new Font("Ink Free", 12),
+								new SolidBrush(v.color),
+								v.point.X, v.point.Y,
+								new StringFormat()
+								{ Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+						}
+						++it;
+					}
+
+				}
+				workingArea.Refresh();
+			}
+		}
+
+		private void workingArea_MouseUp(object sender, MouseEventArgs e)
+		{
+			mouseIsDown = false;
+		}
+	}
+}
+
+
+
